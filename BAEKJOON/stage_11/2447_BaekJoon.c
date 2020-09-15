ae3fb1c5a710f27312020b7ef870cf3f09a60b38
hyungraelee <70879607+hyungraelee@users.noreply.github.com>
2020-09-15 23:17:23 +0900

    200915 [BOJ] 문제 풀기

--- BAEKJOON/stage_11/2447_BaekJoon.c
+++ BAEKJOON/stage_11/2447_BaekJoon.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void init_arr(int, char **);
 void set_Star(int, int, int, char **);
 
 int main()
@@ -9,50 +10,50 @@ int main()
     scanf("%d", &n);
 
     char **arr; 
-    arr = (char **)malloc(sizeof(char*) * (n + 1));
+    arr = (char **)malloc(sizeof(char*) * n);
     for (int p = 0 ; p < n ; p++)
-    {
         arr[p] = (char *)malloc(sizeof(char) * (n + 1));
-    }
 
-
+    init_arr(n, arr);
+    
     set_Star(0, 0, n, arr);
     
     for (int i = 0 ; i < n ; i++)
-    {
-        for (int j = 0 ; j < n ; j++)
-        {
-            printf("%c", arr[i][j]);
-        }
-        printf("\n");
-    }
+        printf("%s\n", arr[i]);
 
     for (int i = 0 ; i < n ; i++)
-    {
         free(arr[i]);
-    }
     free(arr);
 
     return 0;
 }
 
+void init_arr(int n, char** arr)
+{
+    for (int i = 0 ; i < n ; i++)
+        for (int j = 0 ; j <= n ; j++)
+            if (j == n)
+                arr[i][j] = 0;
+            else
+                arr[i][j] = ' ';
+    return ;
+}
+
 void set_Star(int r, int c, int n, char **arr)
 {
     if (n == 1)
+    {
         arr[r][c] = '*';
         return ;
+    }
 
     int m = n / 3;
     for (int i = 0 ; i < 3 ; i++)
-    {
         for (int j = 0 ; j < 3 ; j++)
-        {
             if (i == 1 && j == 1)
                 ;
             else
                 set_Star(r + i*m, c + j*m, m, arr);
-        }
-    }
     
     return ;
 }

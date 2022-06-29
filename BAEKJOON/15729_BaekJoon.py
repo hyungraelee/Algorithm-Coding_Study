fc7691a4b8a0b25ca8e669dfbaa0979b53493520
hyungraelee <hyunlee.dev@gmail.com>
2022-06-29 11:16:59 +0900

    update 15729

---
+++ BAEKJOON/15729_BaekJoon.py
@@ -0,0 +1,14 @@
+def main():
+    N = int(input())
+    targets = list(map(int, input().split()))
+    buttons = [0] * N
+
+    count = 0
+    for idx, target in enumerate(targets):
+        if buttons[idx] != target:
+            count += 1
+            buttons[idx:idx+3] = map(lambda x: not x, buttons[idx:idx+3])
+    print(count)
+
+if __name__ == "__main__":
+    main()
